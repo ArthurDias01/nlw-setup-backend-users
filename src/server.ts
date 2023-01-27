01874b44a03e84fa53348d6be09514e3ca106068
Arthur Dias <arthursantos01@gmail.com>
2023-01-27 10:08:05 -0300

    fix: allowed all origins for cors -temporarily

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const app = Fastify();
 
 
 app.register(cors, {
-  origin: ["http://localhost:5173"],
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
