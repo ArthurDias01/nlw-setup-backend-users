5c7eb4a7aad6a3230e5c41786a9bbbcc087afa3c
Arthur Dias <arthursantos01@gmail.com>
2023-01-27 15:23:26 -0300

    added tsup lib to build

--- src/server.ts
+++ src/server.ts
@@ -27,7 +27,6 @@ app.register(appRoutes);
 app.listen({
   port: 3333,
   host: "0.0.0.0",
-}).then((address) => {
-  console.log('address', address)
+}).then(() => {
   console.log("Server is running on port 3333!");
 })
