2a1c0e4ca73af39a24f217a9145c4895f2b944a5
Arthur Dias <arthursantos01@gmail.com>
2023-01-27 09:57:20 -0300

    fix: reconfigured options for cors policy - adding localhost

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,12 @@ import { appRoutes } from './routes';
 const app = Fastify();
 
 
-app.register(cors);
+app.register(cors, {
+  origin: ["http://localhost:5173"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+});
 app.register(appRoutes);
 
 
